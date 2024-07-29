d76376e0ddf5d943554c1071f211626caead6830
yunX-yv <2099330740@qq.com>
2024-07-30 00:49:59 +0800

    add wifi tools

--- SDK/ESP8089-SPI/spi_stub.c
+++ SDK/ESP8089-SPI/spi_stub.c
@@ -3,9 +3,9 @@
 
 /* https://www.signal.com.tr/pdf/cat/8n-esp8266_spi_reference_en_v1.0.pdf */
 
-#define SPI_FREQ (10000000)
+//#define SPI_FREQ (10000000)
 //#define SPI_FREQ (20000000)                             //  1. 22.5Mhz     2. 45Mhz
-//#define SPI_FREQ (30000000)                             //  1. 22.5Mhz     2. 45Mhz
+#define SPI_FREQ (30000000)                             //  1. 22.5Mhz     2. 45Mhz
 
 //Below are for spi HZ 22.5M
 #if (SPI_FREQ == 30000000)
